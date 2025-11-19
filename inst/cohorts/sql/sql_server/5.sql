cd7a8c6e044baed26f2dc46b3887cfc5ddfc5b5b
Maaike van Swieten <31585951+MaaikevS@users.noreply.github.com>
2025-11-19 15:58:05 +0100

    Rewritten queries

--- inst/cohorts/sql/sql_server/5.sql
+++ inst/cohorts/sql/sql_server/5.sql
@@ -10,35 +10,32 @@
     ON co.person_id = pe.person_id
   JOIN omopcdm.concept_ancestor ca
     ON ca.descendant_concept_id = co.condition_concept_id
-    JOIN (
-    WITH included_measurements AS (
-      SELECT descendant_concept_id AS measurement_concept_id
-      FROM omopcdm.concept_ancestor
-      WHERE ancestor_concept_id IN (1633650)
-    ),
-    excluded_measurements AS (
-      SELECT descendant_concept_id AS measurement_concept_id
-      FROM omopcdm.concept_ancestor
-      WHERE ancestor_concept_id IN (1634876, 1635566)
-    ),
-    allowed_measurements AS (
-      SELECT measurement_concept_id
-      FROM included_measurements
-      WHERE measurement_concept_id NOT IN (SELECT measurement_concept_id FROM excluded_measurements)
-    )
-    SELECT m.person_id, m.measurement_date, m.measurement_concept_id
-    FROM omopcdm.measurement m
-    JOIN omopcdm.condition_occurrence co2
-      ON co2.person_id = m.person_id
-     AND m.measurement_event_id = co2.condition_occurrence_id
-    JOIN allowed_measurements am
-      ON am.measurement_concept_id = m.measurement_concept_id
-    WHERE EXTRACT(YEAR FROM co2.condition_start_date) in (2019)
+    LEFT JOIN (
+      SELECT 
+          m.person_id,
+          m.measurement_date,
+          m.measurement_concept_id
+      FROM omopcdm.measurement m
+      JOIN omopcdm.condition_occurrence co2
+        ON m.person_id = co2.person_id
+       AND m.measurement_event_id = co2.condition_occurrence_id
+      JOIN (
+          SELECT descendant_concept_id AS measurement_concept_id
+          FROM omopcdm.concept_ancestor
+          WHERE ancestor_concept_id IN (1633650)
+            AND descendant_concept_id NOT IN (
+              SELECT descendant_concept_id
+              FROM omopcdm.concept_ancestor
+              WHERE ancestor_concept_id IN (1634876, 1635566)
+            )
+      ) allowed
+        ON allowed.measurement_concept_id = m.measurement_concept_id
+      WHERE EXTRACT(YEAR FROM co2.condition_start_date) IN (2019)
   ) m
-  ON m.person_id = co.person_id
+    ON m.person_id = co.person_id
   WHERE ca.ancestor_concept_id IN (4112853)
     AND co.condition_concept_id NOT IN (44500370,4301516,36543333,36564967,36403034,36403041,36403047,36403017,4001315,4001670,36403027,36403064,36403075,36517431,36529758,36537608,36546703,36557659,36717217,42512152,42512201,44499447,44499563,44499751,44500653,44501092,44501151,44501152,44501276,44501353,44501356,44501446,44501633,44501854,44501955,44501957,44502178,44502714,44502954,44503030,44503556)
-    AND pe.gender_concept_id in (8532)
-    AND EXTRACT(YEAR FROM co.condition_start_date) in (2019)
+    AND pe.gender_concept_id IN (8532)
+    AND EXTRACT(YEAR FROM co.condition_start_date) IN (2019)
       AND m.measurement_date BETWEEN co.condition_start_date - INTERVAL '30' DAY
   AND co.condition_start_date + INTERVAL '30' DAY;
--- inst/cohorts/sql/sql_server/7.sql
+++ inst/cohorts/sql/sql_server/7.sql
@@ -12,12 +12,12 @@
     ON ca.descendant_concept_id = co.condition_concept_id
     INNER JOIN omopcdm.measurement m
     ON m.person_id = co.person_id
-    AND m.measurement_concept_id IN (35917793,35918406)
-    AND m.value_as_concept_id IN (35935003,35919678)
-    AND m.measurement_date BETWEEN co.condition_start_date - INTERVAL '30' DAY
-    AND co.condition_start_date + INTERVAL '30' DAY
+   AND m.measurement_concept_id IN (35917793,35918406)
+   AND m.value_as_concept_id IN (35935003,35919678)
+   AND m.measurement_date BETWEEN co.condition_start_date - INTERVAL '30' DAY
+                            AND co.condition_start_date + INTERVAL '30' DAY
   WHERE ca.ancestor_concept_id IN (4112853)
     AND co.condition_concept_id NOT IN (44500370,4301516,36543333,36564967,36403034,36403041,36403047,36403017,4001315,4001670,36403027,36403064,36403075,36517431,36529758,36537608,36546703,36557659,36717217,42512152,42512201,44499447,44499563,44499751,44500653,44501092,44501151,44501152,44501276,44501353,44501356,44501446,44501633,44501854,44501955,44501957,44502178,44502714,44502954,44503030,44503556)
-    AND pe.gender_concept_id in (8532)
-    AND EXTRACT(YEAR FROM co.condition_start_date) in (2019)
+    AND pe.gender_concept_id IN (8532)
+    AND EXTRACT(YEAR FROM co.condition_start_date) IN (2019)
     ;
